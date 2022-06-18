c1fdbb7ac4ba4030a95042463c7fac535290777d
Blake Sartin <blake.sartin8@gmail.com>
2022-06-18 15:25:17 -0700

    install material ui icons, add logo and email, and phone

--- portfolio/src/components/Topbar.js
+++ portfolio/src/components/Topbar.js
@@ -1,9 +1,24 @@
 import "./Topbar.scss"
+import {Phone, Mail} from '@material-ui/icons'
 
 export default function Topbar() {
   return(
     <div className="topbar">
-      
+      <div className="wrapper">
+      <div className="left">
+        <a href="#intro" className="logo">B.</a>
+        <div className="itemcontainer">
+          <Phone className="icon"/>
+          <span>778-384-2705</span>
+          </div>
+          <div className="itemcontainer"></div>
+          <Mail className="icon"/>
+          <span>blake.sartin8@gmail.com</span>
+          </div>
+        </div>
+      <div className="right">
+     
+        </div>
       </div>
   )
 }
