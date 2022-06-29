b2d0320c402b6941fc3fe29f0f4ea8ca35bac918
Blake Sartin <blake.sartin8@gmail.com>
2022-06-28 17:51:47 -0700

    add portfolio page styling

--- portfolio/src/components/Portfolio.js
+++ portfolio/src/components/Portfolio.js
@@ -1,8 +1,37 @@
-import "./Portfolio.scss"
+import "./Portfolio.scss";
+import {PhoneAndroid} from '@material-ui/icons'
 
 export default function Portfolio() {
-  return(
+  return (
     <div className="portfolio" id="portfolio">
-      </div>
-  )
+      <h1>Portfolio</h1>
+
+      <ul>
+        <li className="active">Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Design</li>
+
+      </ul>
+      <div className="container">
+        <div className="item">
+          <PhoneAndroid className="icon"/>
+          <h3>Dating App</h3>
+          </div>
+          <div className="item">
+          <PhoneAndroid className="icon"/>
+          <h3>Dating App</h3>
+          </div>
+          <div className="item">
+          <PhoneAndroid className="icon"/>
+          <h3>Dating App</h3>
+          </div>
+          <div className="item">
+          <PhoneAndroid className="icon"/>
+          <h3>Dating App</h3>
+          </div>
+        </div>
+        
+    </div>
+  );
 }
