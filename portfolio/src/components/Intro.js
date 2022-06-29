c73ec2f01eaa4877009bd888e99ac51728d80fe8
Blake Sartin <blake.sartin8@gmail.com>
2022-06-28 17:05:19 -0700

    add intro styling and animations

--- portfolio/src/components/Intro.js
+++ portfolio/src/components/Intro.js
@@ -1,8 +1,26 @@
-import "./Intro.scss"
+import "./Intro.scss";
+import { ArrowDownward } from "@material-ui/icons";
 
 export default function Intro() {
-  return(
+  return (
     <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/kindpng_3371936.png" alt="" />
+        </div>
       </div>
-  )
+      <div className="right">
+      <div className="wrapper">
+        <h2>Hello, My name is </h2>
+        <h1>Blake Sartin</h1>
+        <h3>
+          Full-Stack Developer<span>Designer</span>
+        </h3>
+      </div>
+      <a href="#portfolio">
+        <ArrowDownward className="icon" />
+      </a>
+      </div>
+    </div>
+  );
 }
