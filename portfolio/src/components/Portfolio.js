ce707f3a3736b37ae35dff06a87e3887ece5c43a
Blake Sartin <blake.sartin8@gmail.com>
2022-06-30 14:09:46 -0700

    add dummy data and finished portfolio styling and dynamic rendering

--- portfolio/src/components/Portfolio.js
+++ portfolio/src/components/Portfolio.js
@@ -1,10 +1,17 @@
 import "./Portfolio.scss";
 import { PhoneAndroid } from "@material-ui/icons";
 import PortfolioList from "./Portfolio_List/PortfolioList";
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import {
+  featuredPortfolio,
+  webPortfolio,
+  mobilePortfolio,
+  apiPortfolio,
+  designPortfolio,
+} from ".//..//data";
 export default function Portfolio() {
   const [selected, SetSelected] = useState("featured");
+  const [data, setData] = useState([]);
 
   const list = [
     {
@@ -25,7 +32,33 @@ export default function Portfolio() {
       id: "design",
       title: "Design",
     },
+    {
+      id: "Api",
+      title: "Api",
+    },
   ];
+
+  useEffect(() => {
+    switch (selected) {
+      case "featured":
+        setData(featuredPortfolio);
+        break;
+      case "web":
+        setData(webPortfolio);
+        break;
+      case "mobile":
+        setData(mobilePortfolio);
+        break;
+      case "Api":
+        setData(apiPortfolio);
+        break;
+      case "design":
+        setData(designPortfolio);
+        break;
+      default:
+        setData(featuredPortfolio);
+    }
+  }, [selected]);
 
   return (
     <div className="portfolio" id="portfolio">
@@ -42,22 +75,14 @@ export default function Portfolio() {
         ))}
       </ul>
       <div className="container">
-        <div className="item">
-          <PhoneAndroid className="icon" />
-          <h3>Dating App</h3>
-        </div>
-        <div className="item">
-          <PhoneAndroid className="icon" />
-          <h3>Dating App</h3>
-        </div>
-        <div className="item">
-          <PhoneAndroid className="icon" />
-          <h3>Dating App</h3>
-        </div>
-        <div className="item">
-          <PhoneAndroid className="icon" />
-          <h3>Dating App</h3>
-        </div>
+        {data.map((d) => (
+          <div className="item">
+            <img 
+            src={d.img} 
+            alt=""/>
+            <h3>{d.title}</h3>
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ portfolio/src/data.js
@@ -0,0 +1,108 @@
+export const featuredPortfolio = [
+
+  {
+    id:1,
+    title: "Dating App",
+    img:"https://media.istockphoto.com/photos/heart-love-on-a-mobile-phone-picture-id1034417688?k=20&m=1034417688&s=612x612&w=0&h=6wKoSVdA78HS22vYs9JcCWdcQR7grdQ7-f2k1zaLums=",
+  },
+  {
+    id:2,
+    title: "Web App",
+    img:"https://cdn.dribbble.com/users/1753227/screenshots/16784743/media/a2084c7185909024af790e4a93241b42.png?compress=1&resize=400x300&vertical=top",
+  },
+  {
+    id:3,
+    title: "Api",
+    img:"https://cdn.dribbble.com/users/774375/screenshots/11047506/media/9d49544350977efd38dd0c3f2d30c037.png?compress=1&resize=400x300",
+  },
+  {
+    id:4,
+    title: "Design",
+    img:"https://cdn.dribbble.com/users/110372/screenshots/12022846/media/3db54efccd2cf7ac65cb68f8d13d387a.png?compress=1&resize=400x300",
+  }
+]
+
+export const mobilePortfolio = [
+  {
+    id:1,
+    title: "Dating App",
+    img:"https://media.istockphoto.com/photos/heart-love-on-a-mobile-phone-picture-id1034417688?k=20&m=1034417688&s=612x612&w=0&h=6wKoSVdA78HS22vYs9JcCWdcQR7grdQ7-f2k1zaLums=",
+  },{
+    id:2,
+    title: "Dating App",
+    img:"https://media.istockphoto.com/photos/heart-love-on-a-mobile-phone-picture-id1034417688?k=20&m=1034417688&s=612x612&w=0&h=6wKoSVdA78HS22vYs9JcCWdcQR7grdQ7-f2k1zaLums=",
+  },{
+    id:3,
+    title: "Dating App",
+    img:"https://media.istockphoto.com/photos/heart-love-on-a-mobile-phone-picture-id1034417688?k=20&m=1034417688&s=612x612&w=0&h=6wKoSVdA78HS22vYs9JcCWdcQR7grdQ7-f2k1zaLums=",
+  },
+
+]
+export const webPortfolio = [
+  {
+    id:1,
+    title: "Web App",
+    img:"https://cdn.dribbble.com/users/1753227/screenshots/16784743/media/a2084c7185909024af790e4a93241b42.png?compress=1&resize=400x300&vertical=top",
+  },
+  {
+    id:2,
+    title: "Web App",
+    img:"https://cdn.dribbble.com/users/1753227/screenshots/16784743/media/a2084c7185909024af790e4a93241b42.png?compress=1&resize=400x300&vertical=top",
+  },
+  {
+    id:3,
+    title: "Web App",
+    img:"https://cdn.dribbble.com/users/1753227/screenshots/16784743/media/a2084c7185909024af790e4a93241b42.png?compress=1&resize=400x300&vertical=top",
+  },
+  {
+    id:4,
+    title: "Web App",
+    img:"https://cdn.dribbble.com/users/1753227/screenshots/16784743/media/a2084c7185909024af790e4a93241b42.png?compress=1&resize=400x300&vertical=top",
+  },
+]
+
+export const apiPortfolio = [
+  {
+    id:1,
+    title: "Api",
+    img:"https://cdn.dribbble.com/users/774375/screenshots/11047506/media/9d49544350977efd38dd0c3f2d30c037.png?compress=1&resize=400x300",
+  },
+  {
+    id:2,
+    title: "Api",
+    img:"https://cdn.dribbble.com/users/774375/screenshots/11047506/media/9d49544350977efd38dd0c3f2d30c037.png?compress=1&resize=400x300",
+  },
+  {
+    id:3,
+    title: "Api",
+    img:"https://cdn.dribbble.com/users/774375/screenshots/11047506/media/9d49544350977efd38dd0c3f2d30c037.png?compress=1&resize=400x300",
+  },
+  {
+    id:4,
+    title: "Api",
+    img:"https://cdn.dribbble.com/users/774375/screenshots/11047506/media/9d49544350977efd38dd0c3f2d30c037.png?compress=1&resize=400x300",
+  },
+]
+
+export const designPortfolio = [
+  {
+    id:1,
+    title: "Design",
+    img:"https://cdn.dribbble.com/users/110372/screenshots/12022846/media/3db54efccd2cf7ac65cb68f8d13d387a.png?compress=1&resize=400x300",
+  },
+  {
+    id:2,
+    title: "Design",
+    img:"https://cdn.dribbble.com/users/110372/screenshots/12022846/media/3db54efccd2cf7ac65cb68f8d13d387a.png?compress=1&resize=400x300",
+  },
+  {
+    id:3,
+    title: "Design",
+    img:"https://cdn.dribbble.com/users/110372/screenshots/12022846/media/3db54efccd2cf7ac65cb68f8d13d387a.png?compress=1&resize=400x300",
+  },
+  {
+    id:4,
+    title: "Design",
+    img:"https://cdn.dribbble.com/users/110372/screenshots/12022846/media/3db54efccd2cf7ac65cb68f8d13d387a.png?compress=1&resize=400x300",
+  }
+]
