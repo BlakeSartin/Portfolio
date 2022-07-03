15a66aa413655f9179215b780baf61ac810bfa92
Blake Sartin <blake.sartin8@gmail.com>
2022-07-03 15:42:53 -0700

    fixed dummy data to include unique icons

--- portfolio/src/components/Works.js
+++ portfolio/src/components/Works.js
@@ -1,5 +1,5 @@
 import "./Works.scss";
-import { PhoneAndroid, ArrowRight, ArrowLeft } from "@material-ui/icons";
+import { PhoneAndroid, ArrowRight, ArrowLeft, Computer, Android,  FormatPaint } from "@material-ui/icons";
 import { useState } from "react";
 
 export default function Works() {
@@ -11,24 +11,28 @@ export default function Works() {
       id: "1",
       title: "Web Design",
       desc: "asldkjgbasdflvkjbazdsfvlkjhb",
+      icon:<Computer />,
       img: "https://cdn.dribbble.com/users/1753227/screenshots/16784743/media/a2084c7185909024af790e4a93241b42.png?compress=1&resize=400x300&vertical=top",
     },
     {
       id: "2",
       title: "Mobile App",
       desc: "dgakjlgflksadfg",
+      icon:<Android />,
       img: "https://media.istockphoto.com/photos/heart-love-on-a-mobile-phone-picture-id1034417688?k=20&m=1034417688&s=612x612&w=0&h=6wKoSVdA78HS22vYs9JcCWdcQR7grdQ7-f2k1zaLums=",
     },
     {
       id: "3",
       title: "Api",
       desc: "asldkjgbasdflvkjbazdsfvlkjhb",
+      icon:<PhoneAndroid />,
       img: "https://cdn.dribbble.com/users/774375/screenshots/11047506/media/9d49544350977efd38dd0c3f2d30c037.png?compress=1&resize=400x300",
     },
     {
       id: "4",
       title: "Design",
       desc: "asldkjgbasdflvkjbazdsfvlkjhb",
+      icon:<FormatPaint />,
       img: "https://cdn.dribbble.com/users/110372/screenshots/12022846/media/3db54efccd2cf7ac65cb68f8d13d387a.png?compress=1&resize=400x300",
     },
   ];
@@ -47,7 +51,7 @@ export default function Works() {
               <div className="left">
                 <div className="leftContainer">
                   <div className="imgContainer">
-                    <PhoneAndroid />
+                    {d.icon}
                   </div>
                   <h2>{d.title}</h2>
                   <p>{d.desc}</p>
