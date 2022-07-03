fc0f12f7b41b65fcff1b3505798a8495d7164816
Blake Sartin <blake.sartin8@gmail.com>
2022-07-03 15:35:01 -0700

    add slider animations and handleClick function, and dummy data for works

--- portfolio/src/components/Works.js
+++ portfolio/src/components/Works.js
@@ -1,33 +1,68 @@
-import "./Works.scss"
+import "./Works.scss";
 import { PhoneAndroid, ArrowRight, ArrowLeft } from "@material-ui/icons";
+import { useState } from "react";
 
 export default function Works() {
 
-  return(
+  const [currentSlide, setCurrentSlide] = useState(0)
+
+  const data = [
+    {
+      id: "1",
+      title: "Web Design",
+      desc: "asldkjgbasdflvkjbazdsfvlkjhb",
+      img: "https://cdn.dribbble.com/users/1753227/screenshots/16784743/media/a2084c7185909024af790e4a93241b42.png?compress=1&resize=400x300&vertical=top",
+    },
+    {
+      id: "2",
+      title: "Mobile App",
+      desc: "dgakjlgflksadfg",
+      img: "https://media.istockphoto.com/photos/heart-love-on-a-mobile-phone-picture-id1034417688?k=20&m=1034417688&s=612x612&w=0&h=6wKoSVdA78HS22vYs9JcCWdcQR7grdQ7-f2k1zaLums=",
+    },
+    {
+      id: "3",
+      title: "Api",
+      desc: "asldkjgbasdflvkjbazdsfvlkjhb",
+      img: "https://cdn.dribbble.com/users/774375/screenshots/11047506/media/9d49544350977efd38dd0c3f2d30c037.png?compress=1&resize=400x300",
+    },
+    {
+      id: "4",
+      title: "Design",
+      desc: "asldkjgbasdflvkjbazdsfvlkjhb",
+      img: "https://cdn.dribbble.com/users/110372/screenshots/12022846/media/3db54efccd2cf7ac65cb68f8d13d387a.png?compress=1&resize=400x300",
+    },
+  ];
+
+  const handleClick = (direction) => {
+    direction === "left" ? setCurrentSlide(currentSlide > 0 ? currentSlide -1 : data.length-1) : 
+    setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0)
+  }
+
+  return (
     <div className="works" id="works">
-      <div className="slider">
-        <div className="container">
-          <div className="item">
-            <div className="left">
-              <div className="leftContainer">
-                <div className="imgContainer">
-                  <PhoneAndroid/>
+      <div className="slider" style={{transform:`translateX(-${currentSlide*100}vw)`}}>
+        {data.map((d) => (
+          <div className="container">
+            <div className="item">
+              <div className="left">
+                <div className="leftContainer">
+                  <div className="imgContainer">
+                    <PhoneAndroid />
                   </div>
-                  <h2>Title</h2>
-                  <p>Lorem ipsum alfiogjhas asldkfj hsdlfi jbasdl fjkhbasdl jbasli gjb</p>
+                  <h2>{d.title}</h2>
+                  <p>{d.desc}</p>
                   <span>Projects</span>
                 </div>
               </div>
-            <div className="right">
-              <img src="https://media.istockphoto.com/photos/keeping-up-with-modern-advancements-picture-id1081869336?k=20&m=1081869336&s=612x612&w=0&h=0fUoqQ-eRFZXWuvJ3GIVrbmYHAzKEIg6pcu2fhbXQCw="
-              alt=""
-              />
+              <div className="right">
+                <img src={d.img} alt="" />
               </div>
             </div>
           </div>
-        </div>
-        <ArrowLeft className="iconLeft"/>
-        <ArrowRight className="iconRight"/>
+        ))}
       </div>
-  )
+      <ArrowLeft className="iconLeft" onClick={()=>handleClick("left")}/>
+      <ArrowRight className="iconRight" onClick={()=>handleClick()}/>
+    </div>
+  );
 }
