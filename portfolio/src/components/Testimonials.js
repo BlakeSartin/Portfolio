e40d889d08b9295e0fa76c4be8d7b157f0982651
Blake Sartin <blake.sartin8@gmail.com>
2022-07-05 15:46:33 -0700

    add more styling for works and dummy data

--- portfolio/src/components/Testimonials.js
+++ portfolio/src/components/Testimonials.js
@@ -4,28 +4,58 @@ import {
   YouTube,
   Twitter,
   PersonOutlined,
+  Instagram,
 } from "@material-ui/icons";
 
 export default function Testimonials() {
+
+  let data = [
+   {
+      id:2,
+      name: "Tyus Bro",
+      title: "Co-Founder of Best Friends Inc",
+      icon: <Twitter />,
+      desc:"Lorem impsum alghjbav asdkljfhba thlkndfgb sdivjb",
+      featured: false,
+    },
+    {
+      id:1,
+      name: "Kat Teitz",
+      title: "CEO of Fantastic Girlfriend",
+      icon: <YouTube />,
+      desc:"Lorem impsum alghjbav asdkljfhba thlkndfgb sdivjb",
+      featured: true,
+    },
+    {
+      id:3,
+      name: "Max Kim",
+      title: "Co-Founder of Best Friends Inc",
+      icon: <Instagram />,
+      desc:"Lorem impsum alghjbav asdkljfhba thlkndfgb sdivjb",
+      featured: false,
+    },
+
+  ]
   return (
     <div className="testimonials" id="testimonials">
       <h1>Testimonials</h1>
       <div className="container">
-        <div className="card">
+        {data.map((d) => (
+          <div className={d.featured ? "card featured" : "card"}>
           <div className="top">
             <ArrowRightOutlined className="left" />
             <PersonOutlined className="user"/>
-            <YouTube className="right"/>
+            {d.icon}
           </div>
           <div className="center">
-            lorem ipsum sdlgkjasbfgdlkjasbfglakjsfbglakjsfb asfgkjadsolf aldsfk
-            jgnadlfkj gbnasldfkj 
+           {d.desc}
           </div>
           <div className="bottom">
-            <h3>Bob</h3>
-            <h4>CEO of lorem</h4>
+            <h3>{d.name}</h3>
+            <h4>{d.title}</h4>
           </div>
         </div>
+        ))}
       </div>
     </div>
   );
