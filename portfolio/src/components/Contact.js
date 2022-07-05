aa56925fdb3e3709b82a0700bf5ab8109837b637
Blake Sartin <blake.sartin8@gmail.com>
2022-07-05 16:04:51 -0700

    add styling for Contact component

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,9 +1,27 @@
-import React from 'react'
-import "./Contact.scss"
+import React, { useState } from "react";
+import "./Contact.scss";
+import { ContactMail } from "@material-ui/icons";
 
 export default function Contact() {
+  const [message, setMessage] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
   return (
-    <div className="contact" id='contact'> 
+    <div className="contact" id="contact">
+      <div className="left">
+        <ContactMail className="img" />
+      </div>
+      <div className="right">
+        <h2>Contact</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder='"Message'></textarea>
+          <button type="submit">Send</button>
+          {message && <span>Thanks, I'll be in contact ASAP!</span>}
+        </form>
+      </div>
     </div>
-  )
+  );
 }
