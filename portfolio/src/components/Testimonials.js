ffd363fb87f8138a9c12b43d526f3521e08f847b
Blake Sartin <blake.sartin8@gmail.com>
2022-07-05 15:34:56 -0700

    add styling for testimonials

--- portfolio/src/components/Testimonials.js
+++ portfolio/src/components/Testimonials.js
@@ -1,8 +1,32 @@
-import "./Testimonials.scss"
+import "./Testimonials.scss";
+import {
+  ArrowRightOutlined,
+  YouTube,
+  Twitter,
+  PersonOutlined,
+} from "@material-ui/icons";
 
 export default function Testimonials() {
-  return(
+  return (
     <div className="testimonials" id="testimonials">
+      <h1>Testimonials</h1>
+      <div className="container">
+        <div className="card">
+          <div className="top">
+            <ArrowRightOutlined className="left" />
+            <PersonOutlined className="user"/>
+            <YouTube className="right"/>
+          </div>
+          <div className="center">
+            lorem ipsum sdlgkjasbfgdlkjasbfglakjsfbglakjsfb asfgkjadsolf aldsfk
+            jgnadlfkj gbnasldfkj 
+          </div>
+          <div className="bottom">
+            <h3>Bob</h3>
+            <h4>CEO of lorem</h4>
+          </div>
+        </div>
       </div>
-  )
+    </div>
+  );
 }
